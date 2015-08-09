13d232efe2d8009fbcc32ef734498e9e77b1fdf1
aprashar <ankush.prashar@gmail.com>
2015-08-09 01:37:16 -0500

    Modified RestaurantController

--- OnlineReservationPOC/Controllers/RestaurantController.cs
+++ OnlineReservationPOC/Controllers/RestaurantController.cs
@@ -1,11 +1,34 @@
-﻿using System;
+﻿using OnlineReservationPOC.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
+using System.Web.Http;
 
 namespace OnlineReservationPOC.Controllers
 {
-    public class RestaurantController
+    public class RestaurantController :ApiController
     {
+        //api/Restaurants
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        //api/Restaurants/{Id}
+        public HttpResponseMessage Get(int RestaurantId)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        //api/Restaurants
+        [HttpPost]
+        public HttpResponseMessage Get([FromBody] Restaurant selectedRestaurant)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
     }
 }
