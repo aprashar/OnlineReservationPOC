61d8a8a8f343a82ccf3200e5177c28c69fad20be
aprashar <ankush.prashar@gmail.com>
2015-08-09 01:14:46 -0500

    Added Restaurant.cs and RestaurantReservation.cs

---
+++ OnlineReservationPOC/Domain/Restaurant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineReservationPOC.Domain
+{
+    public class Restaurant
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int TotalNumberOfTables { get; set; }
+    }
+}
