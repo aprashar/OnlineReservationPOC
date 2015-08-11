b4a3dc9a565b7808dc19c3c9a780b8a0d5baf784
aprashar <ankush.prashar@gmail.com>
2015-08-11 09:28:37 -0500

    added new entities, resources and controller.

---
+++ OnlineReservationPOC/Controllers/ToDoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace OnlineReservationPOC.Controllers
+{
+    public class ToDoController:ApiController
+    {
+        //api/ToDos
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ OnlineReservationPOC/Resources/ToDo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineReservationPOC.Resources
+{
+    public class ToDo:Resource
+    {
+        public string ActivityName { get; set; }
+        public string ActivityDesc { get; set; }
+    }
+}
